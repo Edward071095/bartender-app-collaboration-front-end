2842be4e0f7306bb8a9305c0acafb1ebf7eb1628
Dyoumans <derrikyoumans416@gmail.com>
2025-03-19 15:39:39 -0400

    adjusted cocktail services js

---
+++ src/services/cocktailService.js
@@ -0,0 +1,112 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/cocktails`;
+
+const index = async () => {
+    try {
+      const res = await fetch(BASE_URL, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const show = async (cocktailId) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${cocktailId}`, {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const create = async (cocktailFormData) => {
+    try {
+      const res = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cocktailFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const createComment = async (cocktailId, commentFormData) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${cocktailId}/comments`, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(commentFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteCocktail = async (cocktailId) => {
+    try {
+      const res = await fetch(`${BASE_URL}/${cocktailId}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async function update(cocktailId, cocktailFormData) {
+    try {
+      const res = await fetch(`${BASE_URL}/${cocktailId}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(cocktailFormData),
+      });
+      return res.json();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteComment = async (cocktailId, commentId) => {
+    try {
+    const res = await fetch(`${BASE_URL}/${cocktailId}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(commentId),
+        });
+        return res.json();
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+  
+  export { 
+    index,
+    show,
+    create,
+    createComment,
+    deleteCocktail,
+    update,
+    deleteComment,
+  };
